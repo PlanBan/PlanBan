f3904c72cc99600f4b4fb63e76e9cc621730a42a
main <162069590+PlanBan@users.noreply.github.com>
2026-09-10 12:05:34 +0200

    Add editable relic collectible logic

---
+++ AbandonedJungleGame/game/scripts/relic.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name JungleRelic
+
+signal collected(value: int)
+
+@export var score_value: int = 100
+@export var enabled: bool = true
+
+func _ready() -> void:
+	monitoring = enabled
+
+func _on_body_entered(body: Node2D) -> void:
+	if not enabled:
+		return
+	if body.is_in_group("player"):
+		enabled = false
+		monitoring = false
+		collected.emit(score_value)
+		$AnimationPlayer.play("collect")
+		await $AnimationPlayer.animation_finished
+		queue_free()
